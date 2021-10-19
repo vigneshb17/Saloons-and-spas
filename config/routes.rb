488c1a27bdfd7b6bc2b539f70b980edc078c8f0b
vignesh balakrishnan <vignesh.balakrishnan@mallow-tech.com>
2021-10-20 00:03:09 +0530

    Add the routes for saloons controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :customers, :controllers => { :sessions => 'sessions' }
+
+  namespace :api, defaults: { format: :json } do
+    resources :saloons, only: :index do
+      member do
+        get :services
+        post :book_service
+        get :booking_reports
+      end
+    end
+  end
 end
